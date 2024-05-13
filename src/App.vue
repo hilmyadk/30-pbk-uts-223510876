f945a985e45c1f95afa1e860566221d30aa077b3
hilmyadk <muhammadhilmyandika@student.uir.ac.id>
2024-05-13 22:21:23 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,97 @@
-<script setup>
-import { ref, onMounted, computed, watch } from 'vue'
+<template>
+	<div class="container">
+	  <header class="header">
+		<nav class="menu">
+		  <ul>
+			<li @click="pilihMenu('todos')" :class="{ 'aktif': menuAktif === 'todos' }">TODOS</li>
+			<li class="separator"></li>
+			<li @click="pilihMenu('posts')" :class="{ 'aktif': menuAktif === 'posts' }">POSTS</li>
+		  </ul>
+		</nav>
+	  </header>
+	  
+	  <div class="konten">
+		<div v-if="menuAktif === 'todos'">
+		  <!-- TodoList component -->
+		  <Todos />
+		</div>
+		<div v-else-if="menuAktif === 'posts'">
+		  <!-- Post component -->
+		  <Posts />
+		</div>
+	  </div>
+	</div>
+  </template>
+  
+<script>
+  import Todos from './components/todos.vue';
+  import Posts from './components/posts.vue';
+  
+  export default {
+	components: {
+	  Todos,
+	  Posts
+	},
+	data() {
+	  return {
+		menuAktif: 'todos'
+	  };
+	},
+	methods: {
+	  pilihMenu(menu) {
+		this.menuAktif = menu;
+	  }
+	}
+  };
+</script>
+  
+<style scoped>
 
-const todos = ref([])
-const name = ref('')
-
-const input_content = ref('')
-
-const todos_asc = computed(() => todos.value.sort((a,b) =>{
-	return a.createdAt - b.createdAt
-}))
-
-watch(name, (newVal) => {
-	localStorage.setItem('name', newVal)
-})
-
-watch(todos, (newVal) => {
-	localStorage.setItem('todos', JSON.stringify(newVal))
-}, {
-	deep: true
-})
-
-const addTodo = () => {
-    if (input_content.value.trim() === '') {
-        return;
-    }
-
-    todos.value.push({
-        content: input_content.value,
-        done: false,
-        editable: false,
-        createdAt: new Date().getTime()
-    });
-};
-
-const toggleEdit = (todo) => {
-    todo.editable = !todo.editable;
-};
-
-const toggleDone = (todo) => {
-    todo.editable = false;
-};
-
-const removeTodo = (todo) => {
-	todos.value = todos.value.filter((t) => t !== todo)
+.container {
+	padding-top: 10px;
+	padding-bottom: 20px;
 }
 
-onMounted(() => {
-	name.value = localStorage.getItem('name') || ''
-	todos.value = JSON.parse(localStorage.getItem('todos')) || []
-})
-</script>
+.header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 20px;
+	padding-bottom: 10px;
+	background-color: #c40300;
+	color: #000000;
+	padding: 10px 20px 20px;
+	font-size: larger;
+}
 
-<template>
-<div style="display: flex; justify-content: center; margin-top: 50px;">
-<div class="container">
-	<div class="app">
-		
-		<section class="greeting">
-			<h2 class="title">
-				Nice to see you, <input type="text" id="name" placeholder="Name here" v-model="name">
-			</h2>
-		</section>
+.menu {
+  justify-self: end; /* Menu akan ditempatkan di ujung kanan */
+}
 
-		<section class="create-todo">
-			<h2>CREATE A TO DO LIST</h2>
-      <br>
+.menu ul {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	display: flex;
+}
 
-			<form id="new-todo-form" @submit.prevent="addTodo">
-				<h3>What do you wanna DO today?</h3>
-				<input 
-					type="text" 
-					name="content" 
-					id="content" 
-					placeholder="e.g. make a delicious food"
-					v-model="input_content" />
-				
-				<input type="submit" value="Add todo" />
-			</form>
-		</section>
+.menu ul li {
+	margin-right: 30px;
+	cursor: pointer;
+	position: relative;
+}
 
-		<section class="todo-list">
-			<h3><b>TO DO LIST : </b></h3>
-			<div class="list" id="todo-list">
+.menu ul li.separator::after {
+	content: "";
+	position: absolute;
+	right: -5px;
+	top: 50%;
+	height: 25px;
+	border-left: 2px solid #000;
+	transform: translateY(-50%);
+}
 
-				<div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
-    				<label>
-        				<input type="checkbox" v-model="todo.done" />
-        				<span :class="`bubble`"></span>
- 					</label>
-
-    			<div class="todo-content">
-        			<input type="text" v-model="todo.content" :readonly="!todo.editable" />
-    			</div>
-
-    			<div class="actions">
-        			<button class="edit" @click="toggleEdit(todo)">Edit</button>
-					<button class="done-button" @click="toggleDone(todo)" v-if="todo.editable">Done</button>
-        			<button class="delete" @click="removeTodo(todo)">Delete</button>
-    			</div>
-				</div>
-
-			</div>
-		</section>
-	</div>
-</div>
-</div>
-</template>
+.menu ul li.aktif {
+	font-weight: bold;
+}
+</style>
